4755930e8f5aa72804446dcf41dd8a3de3c9de46
huyle <huyle210504@gmail.com>
2025-01-03 14:52:26 +0700

    m

--- neovim/invim.sh
+++ neovim/invim.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-NEOVIM_DOWNLOAD_URL="https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
-NEOVIM_RELEASE_PAGE="https://github.com/neovim/neovim/releases"
-
 # check python virtual env if it's eligible for running the python script
 validate_python_env () {
-    if ver=$(python --version); then
+    if _=$(python --version); then
         num_matches=$(pip list | grep -cE 'bs4|lxml|requests')
         case "$num_matches" in
             3)
-                echo Requirements are met!
+                true
                 ;;
             [0-2])
                 echo Python env does not meet requirements! >&2
@@ -25,55 +22,82 @@ validate_python_env () {
 }
 
 # use curl to fetch release page and store it in an shell variable
-check_latest_nvim() {
+get_latest_ver () {
     if validate_python_env; then
         ver=$(python ./neovim/check_latest_nvim.py)
-        echo "$ver"
+        echo ${ver/Nvim}
+    else
+        exit 1
     fi
 
     return 
 }
 
-check_current_nvim () {
-    ver=$(nvim --version | grep -oE v[\.[:digit:]]+ | tr -d v)
-    if [ "$ver" ]; then
-        echo "NeoVim installed! You're using the version $ver"
+get_current_ver () {
+    # inform
+    ver=$(nvim --version | grep -oE v[\.[:digit:]]+)
+    if [ $ver ]; then
+        echo ${ver/v}
     else
-        echo "No NeoVim's installation found"
+        exit 1 
     fi
 
     return
 }
 
-dowload_and_install_nvim () {
+install_nvim () {
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    sudo rm -rf /opt/nvim
+    sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+    patt='export PATH="$PATH:/opt/nvim-linux64/bin"'
+    patt_count=$(grep -c "$patt" ~/.bashrc)
+
+    if (( patt_count == 0 )); then
+        echo "$patt" >> ~/.bashrc
+    fi
+
     return
 }
 
 # drop-down menu
 echo -ne "Options:
 \t0) Quit
-\t1) Check the latest stable version of NeoVim from GitHub
-\t2) Check the current version of used NeoVim
-\t3) Download NeoVim
+\t1) Install NeoVim
 "
 
 # user's input stored in REPLY
 read -p "Select -> "
 
-#
 case "$REPLY" in 
-    0) exit
+    0)  
+        exit 0
         ;;
-    1) check_latest_nvim 
-        exit
+    1)
+        # current version
+        if current=$(get_current_ver); then
+            true
+        fi
+        # latest version
+        if latest=$(get_latest_ver); then
+            true
+        else
+            exit 1
+        fi
+
+        # checking and installing
+        if [ "$current" != "$latest" ]; then
+            echo "Installing NeoVim..."
+            install_nvim
+            echo "Done! You've installed NeoVim version of $(get_current_ver)"
+        elif [ "$current" == "$latest" ]; then
+            echo "You're up to date"
+        else
+            exit 1
+        fi
         ;;
-    2) check_current_nvim
-        exit
-        ;;
-    3) dowload_and_install_nvim
-        exit
-        ;;
-    *) echo "Invalid entry" >&2
+    *)  
+        echo Invalid entry >&2
         exit 1
         ;;
 esac
