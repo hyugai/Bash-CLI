00603ad8c7887b9ec7a29a61bed84c57149a893f
huyle <huyle210504@gmail.com>
2025-01-07 19:39:31 +0700

    m

--- pyrun/pyrun.sh
+++ pyrun/pyrun.sh
@@ -1,35 +1,57 @@
 #!/bin/bash
-export pyrun_path=
+pyrun_path="$HOME/.pyrun"
+
+if ! [ -f "$pyrun_path" ]; then
+    touch "$pyrun_path"
+fi
+
+_log_path() {
+    if (($(grep -c "$1" "$pyrun_path") == 0)); then
+        echo "$1" >> "$pyrun_path"
+    fi
+
+    return
+}
 
 validate_python_file() {
-	if [ -e "$1" ] && [ -f "$1" ]; then
+	if [ -f "$1" ]; then
 		if [[ "$1" =~ ^.+py$ ]]; then
             true
 		else
-            echo "Not a Python file!" >&2
-            exit 1
+			echo "Not a file!" >&2
+			exit 1
 		fi
 	else
-        echo "Invalid path! (path: $1)" >&2
-        exit 1
+		echo "Invalid path! (path: $1)" >&2
+		exit 1
 	fi
 
     return
 }
 
 case "$1" in
-    # validate the $default_path if there's no specified argument
     "")
-        if validate_python_file "$pyrun_path"; then
-            echo "python $pyrun_path"
+        last_used_path=$(tail -n 1 "$pyrun_path")
+        if [ "$last_used_path" ] && validate_python_file "$last_used_path"; then
+            echo "python $last_used_path"
+        else
+            echo "No path are found or valid!" >&2 
+            exit 1
         fi
         ;;
-    -p|--path)
-        if ! [ "$2" ]; then
-            echo "No specified path!" >&2
-            exit 1
-        elif validate_python_file "$2"; then
-            echo OK
-        fi
+    *py)
+        validate_python_file "$1"
+        echo "python $1"
+        ;;
+    -d|--set-default)
+        validate_python_file "$2"
+        _log_path "$2"
+        ;;
+    -u|--unset-default)
+        true
+        ;;
+    *)
+        echo "Invalid argument!" >&2
+        exit 1
         ;;
 esac
