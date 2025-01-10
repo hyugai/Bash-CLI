fc7de4132a2b7ea8954a259030961a970d49d149
huyle <huyle210504@gmail.com>
2025-01-10 21:20:24 +0700

    m

--- pyrun/pyrun.sh
+++ pyrun/pyrun.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
-pyrun_path="$HOME/.pyrun"
+PYRUN_PATH="$HOME/.pyrun"
+LIMITED_NUM_PATHS=50
 
-if ! [ -f "$pyrun_path" ]; then
-    touch "$pyrun_path"
+if ! [ -f "$PYRUN_PATH" ]; then
+    touch "$PYRUN_PATH"
 fi
 
 _log_path() {
-    if (($(grep -c "$1" "$pyrun_path") == 0)); then
-        echo "$1" >> "$pyrun_path"
+    if (($(grep -c "$1" "$PYRUN_PATH") == 0)); then
+        echo "$1" >> "$PYRUN_PATH"
     fi
 
     return
 }
+
+#TODO
+_log_path_with_name() {
+    if (($(grep -c "$1=$2" "$PYRUN_PATH") == 0)); then
+        echo "$1" >> "$PYRUN_PATH"
+    else
+        read -rp "This name's been used, do you want to overwrite it? [y/n]"
+        case "$REPLY" in
+            y)
+                echo "$1=$2" >> "$PYRUN_PATH"
+                ;;
+            n)
+                command ...
+                ;;
+            *)
+                command ...
+                ;;
+        esac
+    fi
+
+    return
+}
+
+_truncate_exceeded_paths() {
+    num_paths=$(wc -l "$PYRUN_PATH" | grep -oE '[[:digit:]]+')
+    if (("$num_paths" > "$1")); then
+        exceeded_paths=$(("$num_paths" - "$1"))
+        sed -i "1,${exceeded_paths}d" "$PYRUN_PATH"
+    fi
+    
+    return
+}
+
+#TODO
+_find_path_by_name() {
+    return
+}
+
+#TODO
 
 validate_python_file() {
 	if [ -f "$1" ]; then
@@ -31,7 +71,7 @@ validate_python_file() {
 
 case "$1" in
     "")
-        last_used_path=$(tail -n 1 "$pyrun_path")
+        last_used_path=$(tail -n 1 "$PYRUN_PATH")
         if [ "$last_used_path" ] && validate_python_file "$last_used_path"; then
             echo "python $last_used_path"
         else
@@ -47,8 +87,18 @@ case "$1" in
         validate_python_file "$2"
         _log_path "$2"
         ;;
-    -u|--unset-default)
-        true
+    -n|--set-mnemonic-name) 
+        validate_python_file "$2"
+        ;;
+    -l|--set-limit)
+        if [ "$2" ]; then
+            _truncate_exceeded_path "$2"
+        else
+            _truncate_exceeded_paths "$LIMITED_NUM_PATHS"
+        fi
+        ;;
+    -h|--help)
+        echo "More info about this command!"
         ;;
     *)
         echo "Invalid argument!" >&2
